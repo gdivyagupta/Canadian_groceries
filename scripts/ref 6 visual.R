09f2fd03c5ea3652ae86e3ab37cb55df0c7edfca
gdivyagupta <67379893+gdivyagupta@users.noreply.github.com>
2024-11-14 14:04:20 -0500

    visualisations using R

---
+++ scripts/ref 6 visual.R
@@ -0,0 +1,8 @@
+# Set the file path to your CSV file
+file_path <- "/Users/Dell/Downloads/hammer-5-csv"  
+
+# Load the CSV file into a data frame
+data <- read.csv(file_path)
+
+# View the first few rows of the data to confirm it's loaded correctly
+head(data)
